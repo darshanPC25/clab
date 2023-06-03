98170ef41da01edede6f46791069058ab505362e
Paridarshan Sahoo <sahooparidarshan@gmail.com>
2023-06-03 15:33:39 +0530

    LAB6

---
+++ clab/lab6p24.c
@@ -0,0 +1,15 @@
+//WAP in c to print the sum of 1^2+2^2+3^2........n^2.
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int sum=0, num=1, a;
+    printf("Enter The 'n': ");
+    scanf("%d",&a);
+
+    while(num<=a){
+        sum=sum+(pow(num,2));
+        num++;
+    }
+        printf("Sum is %d\n",sum);
+}
---
+++ clab/lab6p25.c
@@ -0,0 +1,1 @@
+//WAP in c to find leap Year between year 1800 to 2050
