741bd449b814185b15b15a5c63308e1f3904bfbf
Paridarshan Sahoo <sahooparidarshan@gmail.com>
2023-06-11 21:46:52 +0530

    Update lab5p20.c

--- clab/lab5p20.c
+++ clab/lab5p20.c
@@ -1,21 +1,27 @@
 //WAP in c to check a given number is aemstrong or not
 #include<stdio.h>
+#include<math.h>
 int main(){
-    int num,sum=0,a,org;
+    int num,sum=0,a,org,i=0;
     printf("Enter a number: ");//153
     scanf("%d",&num);
     org=num;
     while(num>0){
-        a=num%10;
+        i++;
         num/=10;
-        sum=sum+(a*a*a);
-
     }
+    num=org;
+    while(num>0){
+        a=num%10;
+        sum=sum+pow(a,i);
+        num/=10;
+    }                           
     if (sum==org){
         printf("%d is an Armstrong Number",org);
     }
     else{
         printf("%d is not an Armstrong Number",org);
     }
+    return 0;
 
 }
