8e29b836243239c2311b4981bcc734dde1e83a4c
Paridarshan Sahoo <sahooparidarshan@gmail.com>
2023-07-02 13:19:46 +0530

    Create lab8p36.c

---
+++ clab/lab8p36.c
@@ -0,0 +1,22 @@
+//WAP in c to find the greatest number in the array.
+#include<stdio.h>
+int main(){
+    int term,i,temp;
+    int a[100];
+    printf("Enter The Number of Term: ");
+    scanf("%d",&term);
+//Input of Array 
+    for(i=0;i<term;i++){
+        printf("Enter The Elements: ");
+        scanf("%d",&a[i]);
+    }
+//Prosses
+    temp=a[0];
+    for(i=1;i<term;i++){
+        if(temp<a[i])
+            temp=a[i];
+        
+    }
+//Printing of Result
+    printf("%d is the a Greatest.",temp);
+}
