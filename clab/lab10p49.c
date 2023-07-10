d7bd0e5f3b1ea38a8c5335333a7ce37c729a4dcc
Paridarshan Sahoo <sahooparidarshan@gmail.com>
2023-07-10 20:58:54 +0530

    Create lab10p49.c

---
+++ clab/lab10p49.c
@@ -0,0 +1,19 @@
+//WAP in C passes array elements to a function
+#include <stdio.h>
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Array elements: ");
+    printArray(arr, size);
+
+    return 0;
+}
